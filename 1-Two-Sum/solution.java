7224f7c2941387249dee2fc0b68d8796670179fd
itslumia <zhouzefeng2016@gmail.com>
2016-07-24 20:50:18 -0500

    [1. Two Sum][Accepted]committed by LeetCode Extension

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -1,6 +1,16 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] ret = new int[2];
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(target-nums[i])) {
+                ret[0] = map.get(target-nums[i]);
+                ret[1] = i;
+                return ret;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return ret;
     }
-};
+}
