933b6721f53504c66f234bfe7cfad87980795de7
itslumia <zhouzefeng2016@gmail.com>
2016-07-28 19:51:17 -0500

    [342. Power of Four][Accepted]committed by LeetCode Extension

--- 342-Power-of-Four/solution.java
+++ 342-Power-of-Four/solution.java
@@ -1,7 +1,8 @@
 public class Solution {
     public boolean isPowerOfFour(int num) {
-        //if ((num & (num-1)) != 0) return false;
-        //return (num & 0x55555555 == 0);
-        return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
+        if (num < 0) return false;
+        if ((num & (num-1)) != 0) return false;
+        return (num & 0x55555555) != 0;
+        //return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
     }
 }
