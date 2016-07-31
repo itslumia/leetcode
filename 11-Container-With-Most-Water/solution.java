04a7def7c0ba19d2c99b3725e1ec78aabe3badb7
itslumia <zhouzefeng2016@gmail.com>
2016-07-31 10:28:30 -0500

    [11. Container With Most Water][Accepted]committed by LeetCode Extension

---
+++ 11-Container-With-Most-Water/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length-1;
+        int area = 0;
+        while (l < r) {
+            area = Math.max(area, (r-l)*Math.min(height[l], height[r]));
+            if (height[l]<height[r]) l++;
+            else r--;
+        }
+        return area;
+    }
+}
