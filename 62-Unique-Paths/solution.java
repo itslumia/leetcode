5ab58e43b512e798984621789c242b20bc4b2790
itslumia <zhouzefeng2016@gmail.com>
2016-08-01 19:32:59 -0500

    [62. Unique Paths][Accepted]committed by LeetCode Extension

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        int[] ans = new int[n];
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                if (i==0) ans[j]=1;
-                else {
-                    ans[j] += j==0?0:ans[j-1];
-                }
-            }
-        }
-        return ans[n-1];
+        int N = n + m - 2;// how much steps we need to do
+            int k = m - 1; // number of steps that need to go down
+            double res = 1;
+            // here we calculate the total possible path number 
+            // Combination(N, k) = n! / (k!(n - k)!)
+            // reduce the numerator and denominator and get
+            // C = ( (n - k + 1) * (n - k + 2) * ... * n ) / k!
+            for (int i = 1; i <= k; i++)
+                res = res * (N - k + i) / i;
+            return (int)res;
     }
 }
