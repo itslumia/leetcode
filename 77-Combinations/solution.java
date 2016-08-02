f1ace046b08928b3b16ed365364f59357e977b67
itslumia <zhouzefeng2016@gmail.com>
2016-08-02 18:57:39 -0500

    [77. Combinations][Accepted]committed by LeetCode Extension

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -1,18 +1,22 @@
 public class Solution {
-     public static List<List<Integer>> combine(int n, int k) {
-		List<List<Integer>> combs = new ArrayList<List<Integer>>();
-		combine(combs, new ArrayList<Integer>(), 1, n, k);
-		return combs;
-	}
-	public static void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
-		if(k==0) {
-			combs.add(new ArrayList<Integer>(comb));
-			return;
-		}
-		for(int i=start;i<=n;i++) {
-			comb.add(i);
-			combine(combs, comb, i+1, n, k-1);
-			comb.remove(comb.size()-1);
-		}
-	}
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (k < 1 || k > n) return ans;
+        helper(ans, new ArrayList<Integer>(), 1, n, k);
+        return ans;
+    }
+    
+    private void helper(List<List<Integer>> ans, List<Integer> list, int start, int n, int count) {
+        if (count == 0) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        else {
+            for (int i=start; i<=n; i++) {
+                list.add(i);
+                helper(ans, list, i+1, n, count-1);
+                list.remove(list.size()-1);
+            }
+        }
+    }
 }
